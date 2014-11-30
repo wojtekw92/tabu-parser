8eb492d25c12d9f2fb03e59490cfe6a7151bf817
Michał Budzyński <michal@virtualdesign.pl>
2014-11-30 01:01:00 +0100

    :paw_prints: first page parsing worked

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 'use strict';
 
-var request = require('request');
+var jsdom = require('jsdom');
 
 // var cardsPerPage = 12;
 // var totalPages = 534;
 var currentStaringPoint = 0;
 var url = 'http://www.kartabu.com/pl/index.php?first=';
 
-request(url + currentStaringPoint, function(err, req, body){
-  console.log(body);
-});
+jsdom.env(
+  url + currentStaringPoint,
+  [],
+  function(err, win){
+    var forbids;
+    var cards = win.document.querySelectorAll('td[align=center][valign=top]');
+    cards = Array.prototype.slice.call(cards);
+    cards.forEach(function(card){
+      console.log('-----------------------')
+      console.log('Word: ', card.querySelectorAll('.card_top_name')[0].innerHTML);
+      console.log('Dont use:');
+      forbids = card.querySelectorAll('.card_tabu_div');
+      forbids = Array.prototype.slice.call(forbids);
+      forbids.forEach(function(forbid){
+        console.log(forbid.innerHTML);
+      });
+    });
+  }
+);
