e38e9c803d21661567015747364d7e3c806a4ff5
Michał Budzyński <michal@virtualdesign.pl>
2014-11-30 01:01:00 +0100

    :laughing: lint fixes

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+'use strict';
+
 var request = require('request');
 
-var cardsPerPage = 12;
-var totalPages = 534;
+// var cardsPerPage = 12;
+// var totalPages = 534;
 var currentStaringPoint = 0;
 var url = 'http://www.kartabu.com/pl/index.php?first=';
 
